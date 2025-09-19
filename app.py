afaf587843f6c311101b1f8f7db240ba69b91aca
WinniW86 <lockerwin86@gmail.com>
2025-09-19 16:49:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def index():
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
